f1dedd6a8267a9ff9e2fbd62f66c8165b36990f5
Symbioz <35522904+Symbioz@users.noreply.github.com>
2018-01-17 10:16:18 +0100

    ajout du projet GestiBank

---
+++ gestiBank/src/app/layout/gestion-agents/gestion-agents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-agents',
+  templateUrl: './gestion-agents.component.html',
+  styleUrls: ['./gestion-agents.component.scss']
+})
+export class GestionAgentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
