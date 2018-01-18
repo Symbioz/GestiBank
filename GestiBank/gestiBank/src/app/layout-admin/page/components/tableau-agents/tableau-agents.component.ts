b2f951047dfbd9637051e290d027375c0166d7d6
jennifer vincent <vincentjenni@gmail.com>
2018-01-19 00:02:06 +0100

    test variable dansconstructeur

--- GestiBank/gestiBank/src/app/layout-admin/page/components/tableau-agents/tableau-agents.component.ts
+++ GestiBank/gestiBank/src/app/layout-admin/page/components/tableau-agents/tableau-agents.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tableau-agents.component.scss']
 })
 export class TableauAgentsComponent implements OnInit {
+  
+  name : string = "Jennnifer";
+   
+
 
   constructor() { }
 
