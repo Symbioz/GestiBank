b575bc51a244bc8bfc62cb474413a79d662359ea
Symbioz <35522904+Symbioz@users.noreply.github.com>
2018-01-18 16:59:29 +0100

    Le Modal fonctionne !

--- gestiBank/src/app/layout-agent/clients/clients.module.ts
+++ gestiBank/src/app/layout-agent/clients/clients.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule } from '@ngx-translate/core';
 import { ClientsRoutingModule } from './clients-routing.module';
 import {ClientsComponent} from './clients.component';
 import { TableauClientsComponent } from './tableau-clients/tableau-clients.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {ModalComponent} from './modal/modal.component';
+
 
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule, 
     ClientsRoutingModule,
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+     NgbModule.forRoot()
   ],
-  declarations: [ClientsComponent,TableauClientsComponent]
+  declarations: [ClientsComponent,TableauClientsComponent,ModalComponent]
 })
 export class ClientsModule { }
