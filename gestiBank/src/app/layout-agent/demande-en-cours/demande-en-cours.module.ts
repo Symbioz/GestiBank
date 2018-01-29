5def50c0ef49498e9fefb2c402a8c6f5ce5ce43a
Symbioz <35522904+Symbioz@users.noreply.github.com>
2018-01-29 13:30:43 +0100

    etiquette works dans demande-en-cours

--- gestiBank/src/app/layout-agent/demande-en-cours/demande-en-cours.module.ts
+++ gestiBank/src/app/layout-agent/demande-en-cours/demande-en-cours.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DemandeEnCoursRoutingModule } from './demande-en-cours-routing.module';
 import {DemandeEnCoursComponent} from './demande-en-cours.component';
+import { EtiquetteComponent } from './etiquette/etiquette.component';
+import { StatModule } from '../../shared';
+
 @NgModule({
   imports: [
     CommonModule,
-    DemandeEnCoursRoutingModule
+    DemandeEnCoursRoutingModule,
+    StatModule
   ],
-  declarations: [DemandeEnCoursComponent]
+  declarations: [DemandeEnCoursComponent, EtiquetteComponent]
 })
 export class DemandeEnCoursModule { }
