080a6eb7a1eb8064a017b3e4c4b1d317807d00cf
jraes <35526501+jraes@users.noreply.github.com>
2018-02-13 13:32:50 +0100

    ajout d'un attribut 'status'
    l'attribut status de l'objet notification indique l'importance du message (danger/info/succes/warning)

--- GestiBankBackEnd/src/fr/jgj/gestibank/model/Notification.java
+++ GestiBankBackEnd/src/fr/jgj/gestibank/model/Notification.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class Notification {
 	
 	private Long id;
+	private String status;
 	private String message;
 	private Date date;
 	
@@ -13,9 +14,10 @@ public class Notification {
 		super();
 	}
 
-	public Notification(Long id, String message, Date date) {
+	public Notification(Long id, String status, String message, Date date) {
 		super();
 		this.id = id;
+		this.status = status;
 		this.message = message;
 		this.date = date;
 	}
@@ -26,6 +28,13 @@ public class Notification {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 	public String getMessage() {
