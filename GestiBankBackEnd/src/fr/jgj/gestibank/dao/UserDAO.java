27222a9a563c2c1e1f7580fc70da22f945dba896
jraes <35526501+jraes@users.noreply.github.com>
2018-02-13 13:34:48 +0100

    spring

--- GestiBankBackEnd/src/fr/jgj/gestibank/dao/UserDAO.java
+++ GestiBankBackEnd/src/fr/jgj/gestibank/dao/UserDAO.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import fr.jgj.gestibank.model.User;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+
+@Component
 public class UserDAO {
 	
 	//static HashMap<String, User> usersMap = new HashMap<String, User>();
