158a811e3d337206ad410260938d74dcfc682ed6
jraes <35526501+jraes@users.noreply.github.com>
2018-02-16 13:28:25 +0100

    récupération de l'ensemble des opérations d'un compte en particulier
    il n'est pas nécessaire de récupérer l'ensemble des opérations de tous les comptes

--- GestiBankBackEnd/src/fr/jgj/gestibank/dao/IOperationDAO.java
+++ GestiBankBackEnd/src/fr/jgj/gestibank/dao/IOperationDAO.java
@@ -6,7 +6,7 @@ import fr.jgj.gestibank.model.Operation;
 
 public interface IOperationDAO {
 	
-	public List<Operation> getAllOperation();
+	public List<Operation> getAllOperationsByIBAN(String iBAN);
 	
 	public Operation ajoutOperation(Operation operation);
 
