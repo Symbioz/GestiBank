4503c7f579d9890f08141311ca05e37f2fbf71c7
jraes <35526501+jraes@users.noreply.github.com>
2018-02-17 13:15:45 +0100

    suppr commentaires inutiles

--- GestiBankBackEnd/src/fr/jgj/gestibank/rest/resource/CompteRessource.java
+++ GestiBankBackEnd/src/fr/jgj/gestibank/rest/resource/CompteRessource.java
@@ -1,12 +1,19 @@
 package fr.jgj.gestibank.rest.resource;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import fr.jgj.gestibank.model.Compte;
 import fr.jgj.gestibank.model.Operation;
@@ -39,10 +46,53 @@ public class CompteRessource {
 	// CRUD -- READ operation
 	@GET
 	@Path("/{iBAN}/operations")
-	//@Path("/{iBAN}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Operation> getAllOperationsByIBAN(@PathParam("iBAN") String iBAN) {
 		List<Operation> operationList = operationServiceImpl.getAllOperationsByIBAN(iBAN);
 		return operationList;
 	}
+	
+	@GET
+	@Path("/testRecupString")
+	@Produces("text/html")
+    public Response getResultByPassingString(@QueryParam("message") String message) {
+        String output = "Message : "+ message;
+        System.out.println(output);
+        return Response.status(200).entity(output).build();
+    } // >> http://localhost:8080/GestiBankBackEnd/comptes/testRecupString?message=Hello
+	
+	@GET
+	@Path("/testRecupDate2")
+	@Produces("text/html")
+    public Response getResultByPassingDate2(
+    		@QueryParam("date") String date) {  
+		System.out.println("-- testRecupDate2 --");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	    Date dateObj = null;
+		try {
+			dateObj = sdf.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String output = "Date de debut : " + dateObj;
+        System.out.println(output);
+        return Response.status(200).entity(output).build();
+    } 
+	// >> http://localhost:8080/GestiBankBackEnd/comptes/testRecupDate2?date=2018-02-17
+
+	
+//	// CRUD -- READ operation
+//	@Path("/{iBAN}/operations")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String getOperationsByIbanWithParam(
+//		@PathParam("iBAN") String iBAN,
+//		@QueryParam("dateDeb") String dateDeb
+//		) {
+//		List<Operation> operationList = operationServiceImpl.getAllOperationsByIBAN(iBAN);
+//		//System.out.println("Parametres date de debut : " + dateDeb);
+//		return dateDeb;
+//	}
+	
+	
 }
