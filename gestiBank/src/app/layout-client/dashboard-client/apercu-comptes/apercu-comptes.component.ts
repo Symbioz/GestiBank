61cd57e2b1d98efc41ca87adb46ca9054e3109ed
jraes <35526501+jraes@users.noreply.github.com>
2018-02-17 20:33:35 +0100

    modif aperçu-compte

--- gestiBank/src/app/layout-client/dashboard-client/apercu-comptes/apercu-comptes.component.ts
+++ gestiBank/src/app/layout-client/dashboard-client/apercu-comptes/apercu-comptes.component.ts
@@ -1,24 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Component, OnInit, Input } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Compte, Operation, TypeOp } from '../../../../models';
+import { ActivatedRoute, Router } from  '@angular/router';
+import { routerTransition } from '../../../router.animations';
+import { Compte, CompteCourant, CompteRemunere } from '../../../../models';
+import { CompteService } from "../../../../services"; 
 
 @Component({
   selector: 'app-apercu-comptes',
   templateUrl: './apercu-comptes.component.html',
-  styleUrls: ['./apercu-comptes.component.scss']
+  styleUrls: ['./apercu-comptes.component.scss'],
+  providers: [CompteService],
+  animations: [routerTransition()]
 })
 export class ApercuComptesComponent implements OnInit {
 
-    //Gestion du select compte
-  	@Input() comptes: Compte[];
+    //Liste des comptes
+  	comptes: Compte[];
 
-    //Constructeurs
-  	constructor() { }
+    id: number = 1;
+    private sub: any;
+
+    //Constructeur
+  	constructor(private route: ActivatedRoute,
+                private router: Router,
+                private compteService: CompteService) { }
 
     //Init
   	ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+          this.id = params['id'];
+        })
+
+        this.getClientComptes();
   	}
+
+    getClientComptes() {
+        this.compteService.findClientComptes(this.id).subscribe(
+        comptes => {
+          this.comptes = comptes
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+
+    ngOnDestroy(): void { //suppression de l'écouteur lors du déchargement du composant
+      this.sub.unsubscribe();
+    }
 
 }
 
