7b8a0d2339f3188fde153e4e68bd025d407d5177
jraes <35526501+jraes@users.noreply.github.com>
2018-02-20 17:16:10 +0100

    ajout GestiBankBackEnd_SprinjJpa template

---
+++ GestiBankBackEnd/src/fr/jgj/utils/LoadingUtilsTest.java
@@ -0,0 +1,10 @@
+package fr.jgj.utils;
+
+public class LoadingUtilsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//LoadingUtils test= new LoadingUtils()
+	}
+
+}
